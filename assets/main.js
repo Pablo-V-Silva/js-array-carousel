fbc0d4dee54d6f2bcf1b489169baea8875a3cb1c
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-10-15 12:04:07 +0200

    Initial commit
    base sturcture

---
+++ assets/main.js
@@ -0,0 +1,21 @@
+/* Creare un carosello con l'immagine selezionata sulla sinistra del carosello ed il carossello a destra con tanto di bottoni per andare su e giù con le immagini, quando le immagini vengono selezionate ed appaiono a schermo, compare anche una descrizione del luogo che abbiamo selezionato
+
+- selezione dell'immagine
+    SE selezionata
+      appare a sinistra
+- bottone up
+    SE premuto
+      scorre verso l'alto
+- bottone down
+    SE premuto
+      scorre verso il basso
+
+STRUMENTI
+- console.log
+- array
+- for
+- .addEventListner
+- if
+*/
+
+
